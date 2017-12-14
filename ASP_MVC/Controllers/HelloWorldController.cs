b729fcace19c22d6b43d08eb9c3551f701156768
jeannine510 <xujinglin510@163.com>
2017-12-14 16:51:03 +0800

    ASP.net entry project

---
+++ ASP_MVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_MVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action...";
+        //}
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            //return HttpUtility.HtmlEncode("Hello " + name +", ID: " + id);
+            // return "This is the Welcome action method...";
+
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+
+        
+    }
+}
